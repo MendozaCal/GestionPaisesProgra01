0d25e8e8841fd6a47decd2696cc3af7b885532ff
EduardoLopezBe <147556964+EduardoLopezBe@users.noreply.github.com>
2024-04-20 23:43:02 -0500

    Pais

--- GestionPaisesPrograGrupal/Juego.cs
+++ GestionPaisesPrograGrupal/Juego.cs
@@ -8,9 +8,51 @@ namespace GestionPaisesPrograGrupal
 {
     internal class Juego
     {
+        private Pais pais;
+
+        public Juego()
+        {
+            pais = new Pais(1000, 5000, 80); 
+        }
         public void Execute()
         {
+            bool continuar = true;
 
+            while (continuar)
+            {
+                Console.WriteLine("Bienvenido al juego de gestion de paises");
+                Console.WriteLine("Opciones:");
+                Console.WriteLine("1. Pasar turno");
+                Console.WriteLine("2. Ver estadísticas");
+                Console.WriteLine("3. Salir");
+
+                int opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        pais.PasarTurno();
+                        break;
+                    case 2:
+                        pais.VerEstadisticas();
+                        break;
+                    case 3:
+                        Console.WriteLine("Saliendo del juego...");
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida.");
+                        break;
+                }
+
+                if (pais.Felicidad < 20)
+                {
+                    Console.WriteLine("El juego ha terminado por falta de felicidad.");
+                    continuar = false;
+                }
+            }
         }
     }
 }
+    
+
---
+++ GestionPaisesPrograGrupal/Pais.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPaisesPrograGrupal
+{
+    internal class Pais
+    {
+        
+        public int Poblacion { get; set; }
+        public float Dinero { get; set; }
+        public int Felicidad { get; set; }
+        public float TasaImpuestos { get; set; }
+
+        public Salud Salud { get; set; }
+        public Educación Educacion { get; set; }
+        public Seguridad Seguridad { get; set; }
+
+        public Pais(int poblacionInicial, float dineroInicial, int felicidadInicial)
+        {
+            Poblacion = poblacionInicial;
+            Dinero = dineroInicial;
+            Felicidad = felicidadInicial;
+            TasaImpuestos = 0.2f; 
+
+            Salud = new Salud("Salud", 150, 10);
+            Educacion = new Educación("Educación", 100, 10);
+            Seguridad = new Seguridad("Seguridad", 200, 10);
+        }
+        public void PasarTurno()
+        {
+           
+            Dinero -= Salud.GetPrice() + Educacion.GetPrice() + Seguridad.GetPrice();
+
+           
+            Dinero += Poblacion * TasaImpuestos;
+
+            
+            Poblacion += Salud.GetVariationSalud();
+
+            
+            Felicidad += Seguridad.GetVariationSeguridad();
+
+            if (Felicidad < 20)
+            {
+                Console.WriteLine("El juego ha terminado porque la felicidad es demasiado baja.");
+            }
+        }
+
+        public void VerEstadisticas()
+        {
+            Console.WriteLine($"Población: {Poblacion}");
+            Console.WriteLine($"Dinero: {Dinero}");
+            Console.WriteLine($"Felicidad: {Felicidad}");
+        }
+    }
+}
