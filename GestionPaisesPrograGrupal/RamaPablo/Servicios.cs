c4124b55491327106a798a7305750f83830bab49
Pablo Mendoza <asdson5555@gmail.com>
2024-04-20 20:39:46 -0500

    Crear Servicios clase padre

---
+++ GestionPaisesPrograGrupal/RamaPablo/Servicios.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPaisesPrograGrupal
+{
+    internal class Servicios
+    {
+        protected string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public virtual float GetPrice()
+        {
+            return 0;
+        }
+        public virtual int GetVariationSalud()
+        {
+            return 0;
+        }
+        public virtual int GetVariationEducacion()
+        {
+            return 0;
+        }
+        public virtual int GetVariationSeguridad()
+        {
+            return 0;
+        }
+    }
+}
