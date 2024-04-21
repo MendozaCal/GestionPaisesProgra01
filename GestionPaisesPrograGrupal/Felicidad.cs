d9fc3f4ceeec0ab622dfe64141afe7ac8c58843d
Ricardo <147557101+RickParionaChubascos@users.noreply.github.com>
2024-04-20 21:27:32 -0500

    felicidad

---
+++ GestionPaisesPrograGrupal/Felicidad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionPaisesPrograGrupal
+{
+    internal class Felicidad
+    {
+        public int FelicidadN { get; set; }
+        public int VariantFelicidad { get; set; }
+
+        public virtual int GetHappy()
+        {
+            return FelicidadN;
+        }
+
+        public virtual int GetVariationFelicidad()
+        {
+            return VariantFelicidad;
+        }
+
+        public void AffectHappiness(int gastoSeguridad)
+        {
+            if (gastoSeguridad < 20)
+            {
+                FelicidadN -= 10;
+            }
+        }
+    }
+}
